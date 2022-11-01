d46f883b577164bb5d258c867b20e05c22ddf812
Charlie <barehamcharlie@gmail.com>
2022-11-01 18:22:58 +0000

    Put some code into functions

--- main.py
+++ main.py
@@ -2,51 +2,7 @@ import networkx as nx
 from matplotlib import pyplot as plt
 
 
-def verify_routing_tables(network):
-    for node, node_data in network.nodes(data=True):
-        routing_table = node_data["routing_table"]
-        for destination, (next_hop, distance) in routing_table.items():
-            shortest_paths = list(
-                nx.all_shortest_paths(network, source=node, target=destination)
-            )
-            if len(shortest_paths[0]) == 1:
-                return False
-            if all(p[1] != next_hop for p in shortest_paths):
-                return False
-            if len(shortest_paths[0]) - 1 != distance:
-                return False
-
-    return True
-
-
-def main():
-    # generate random connected graph
-    network = nx.gnp_random_graph(10, 0.3)
-    network: nx.Graph = nx.subgraph(
-        network, max(nx.connected_components(network), key=len)
-    )
-
-    for _, data in network.nodes(data=True):
-        data.update({"receive_buffer": [], "routing_table": {}})
-
-    for source, source_data in network.nodes(data=True):
-        for destination in network.neighbors(source):
-            source_data["routing_table"][destination] = destination, 1
-
-        message = {
-            "type": "route_update",
-            "source": source,
-            "routes": [
-                (dest, dist) for dest, (_, dist) in source_data["routing_table"].items()
-            ],
-        }
-        for destination in network.neighbors(source):
-            network.nodes[destination]["receive_buffer"].append(message)
-
-    for node, node_data in network.nodes(data=True):
-        print(node, node_data["routing_table"])
-    print()
-
+def simulate(network):
     loop = True
     while loop:
         loop = False
@@ -79,9 +35,61 @@ def main():
 
             node_data["receive_buffer"].clear()
 
-        for node, node_data in network.nodes(data=True):
-            print(node, node_data["routing_table"])
-        print()
+
+def send_initial_messages(network):
+    for source, source_data in network.nodes(data=True):
+        for destination in network.neighbors(source):
+            source_data["routing_table"][destination] = destination, 1
+
+        message = {
+            "type": "route_update",
+            "source": source,
+            "routes": [
+                (dest, dist) for dest, (_, dist) in source_data["routing_table"].items()
+            ],
+        }
+        for destination in network.neighbors(source):
+            network.nodes[destination]["receive_buffer"].append(message)
+
+
+def verify_routing_tables(network):
+    for node, node_data in network.nodes(data=True):
+        routing_table = node_data["routing_table"]
+        for destination, (next_hop, distance) in routing_table.items():
+            shortest_paths = list(
+                nx.all_shortest_paths(network, source=node, target=destination)
+            )
+            if len(shortest_paths[0]) == 1:
+                return False
+            if all(p[1] != next_hop for p in shortest_paths):
+                return False
+            if len(shortest_paths[0]) - 1 != distance:
+                return False
+
+    return True
+
+
+def main():
+    # generate random connected graph
+    network = nx.gnp_random_graph(10, 0.3)
+    network: nx.Graph = nx.subgraph(
+        network, max(nx.connected_components(network), key=len)
+    )
+
+    for _, data in network.nodes(data=True):
+        data.update({"receive_buffer": [], "routing_table": {}})
+
+    send_initial_messages(network)
+
+    for node, node_data in network.nodes(data=True):
+        print(node, node_data["routing_table"])
+    print()
+
+    simulate(network)
+
+    for node, node_data in network.nodes(data=True):
+        print(node, node_data["routing_table"])
+    print()
 
     print(verify_routing_tables(network))
 
